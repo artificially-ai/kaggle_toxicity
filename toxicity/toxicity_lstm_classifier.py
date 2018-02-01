8112fcb69697b57c198ad58752a65133552644da
Wilder Rodrigues <wilder.rodrigues@ekholabs.ai>
2018-02-01 23:02:11 +0100

    Use stacked Bi-directional LSTMs.

--- toxicity/toxicity_lstm_classifier.py
+++ toxicity/toxicity_lstm_classifier.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, Dropout, Flatten
+from keras.models import Input, Model
+from keras.layers import Dense, Embedding, LSTM, Dropout, concatenate
 from keras.layers.wrappers import Bidirectional
 
 from keras.callbacks import ModelCheckpoint, EarlyStopping
@@ -22,26 +22,31 @@ class ToxicityLSTMClassifier(ToxicityClassifier):
         self.n_lstm_2 = hyper_parameters['lstm_2_dimensions']
         self.drop_lstm = hyper_parameters['lstm_dropout']
 
-        self.dense_1_dimenssions = hyper_parameters['dense_1_dimensions']
+        self.dense_1_dimensions = hyper_parameters['dense_1_dimensions']
         self.dense_dropout = hyper_parameters['dense_dropout']
 
     def build_model(self):
-        model = Sequential()
-        model.add(Embedding(self.unique_words, self.input_dimensions, input_length=self.max_review_length))
-        model.add(Bidirectional(LSTM(self.n_lstm_1, dropout=self.drop_lstm, return_sequences=True)))
-        model.add(Bidirectional(LSTM(self.n_lstm_2, dropout=self.drop_lstm, return_sequences=True)))
+        input_layer = Input(shape=(self.max_review_length,), dtype='int16', name='input')
+        embedding_layer = Embedding(self.unique_words, self.input_dimensions, input_length=self.max_review_length,
+                                    name='embedding_1')(input_layer)
 
-        model.add(Dense(self.dense_1_dimenssions, activation=self.activation_fn))
-        model.add(Dropout(self.dense_dropout))
+        bi_lstm_1 = Bidirectional(LSTM(self.n_lstm_1, dropout=self.drop_lstm, return_sequences=True,
+                                       name='bi_lstm_1'))(embedding_layer)
+        bi_lstm_2 = Bidirectional(LSTM(self.n_lstm_2, dropout=self.drop_lstm, return_sequences=True,
+                                       name='bi_lstm_2'))(embedding_layer)
 
-        model.add(Flatten())
+        concat = concatenate([bi_lstm_1, bi_lstm_2])
 
-        model.add(Dense(self.n_classes, activation='softmax'))
+        dense_layer_1 = Dense(self.dense_1_dimensions, activation=self.activation_fn)(concat)
+        drop_dense_layer_1 = Dropout(self.dense_dropout, name='drop_dense_1')(dense_layer_1)
+        output = Dense(self.n_classes, activation='sigmoid', name='output')(drop_dense_layer_1)
 
-        return model
+        return Model(input_layer, output)
 
     def compile_model(self):
         model = self.build_model()
+        print(model.summary())
+
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
         self.modelCheckPoint = ModelCheckpoint(filepath=self.output_dir + '/weights-lstm-toxicity.hdf5',
@@ -57,8 +62,8 @@ class ToxicityLSTMClassifier(ToxicityClassifier):
         X_train, X_valid, y_train, y_valid, X_test_sub = self.preprocess_data()
 
         model = self.compile_model()
-        model.fit(X_train, y_train, batch_size=self.batch_size, epochs=self.epochs, verbose=2, validation_data=(X_valid, y_valid),
-                  callbacks=[self.modelCheckPoint, self.earlyStopping])
+        model.fit(X_train, y_train, batch_size=self.batch_size, epochs=self.epochs, verbose=2,
+                  validation_data=(X_valid, y_valid), callbacks=[self.modelCheckPoint, self.earlyStopping])
 
         model.save(filepath=self.output_dir + '/model-lstm-toxicity.hdf5')
         y_hat = model.predict(X_test_sub)
