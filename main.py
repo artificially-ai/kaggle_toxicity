c4f1b5e257dce80df95a4acd4be6248f82ce3108
Wilder Rodrigues <wilder.rodrigues@ekholabs.ai>
2018-02-01 17:47:01 +0100

    Fix aws command.

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from subprocess import call
 
 if __name__ == '__main__':
 
-    classifiers = {'cnn' : ToxicityCNNClassifier(), 'lstm' : ToxicityLSTMClassifier() }
+    classifiers = {'cnn': ToxicityCNNClassifier(), 'lstm': ToxicityLSTMClassifier()}
 
     if len(sys.argv) < 2:
         print('Please, pass the model type you want to execute. for example, "cnn" or "lstm".')
         sys.exit(1)
 
-    model_type = sys.argv[1]
-    hyperparameter = 'hyperparams_%s.json' % model_type
+    model_type = sys.argv[1][0]
+    hyperparams = 'hyperparams_%s.json' % model_type
 
-    hyper_parameters = json.load(open('/data/%s' % hyperparameter))
+    hyper_parameters = json.load(open('/data/%s' % hyperparams))
     toxicity = classifiers[model_type]
     toxicity.init(hyper_parameters)
     toxicity.train_model()
@@ -26,4 +26,5 @@ if __name__ == '__main__':
     call("cp /data/config /root/.aws/.".split(sep=' '))
     call("cp /data/credentials /root/.aws/.".split(sep=' '))
 
-    call("aws s3 cp --recursive /ekholabs/toxicity/model_output/" + model_type +" s3://ekholabs-kaggle-models".split(sep=' '))
+    aws_cmd = "aws s3 cp --recursive /ekholabs/toxicity/model_output/%s s3://ekholabs-kaggle-models" % model_type
+    call(aws_cmd.split(sep=' '))
